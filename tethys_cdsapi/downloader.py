7f1602500869aea3884f3893271ed688753a0472
Mike Kittridge <mullenkamp1@gmail.com>
2022-06-16 20:08:30 +1200

    added other products

--- tethys_cdsapi/downloader.py
+++ tethys_cdsapi/downloader.py
@@ -35,7 +35,7 @@ hours = ['00:00', '01:00', '02:00',
 
 file_naming = '{param}_{from_date}-{to_date}_{product}.nc'
 
-available_parameters = {'reanalysis-era5-land': [
+available_variables = {'reanalysis-era5-land': [
             '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
             '2m_temperature', 'evaporation_from_bare_soil', 'evaporation_from_open_water_surfaces_excluding_oceans',
             'evaporation_from_the_top_of_canopy', 'evaporation_from_vegetation_transpiration', 'forecast_albedo', 'lake_bottom_temperature', 'lake_ice_depth', 'lake_ice_temperature',
@@ -51,7 +51,130 @@ available_parameters = {'reanalysis-era5-land': [
             'surface_thermal_radiation_downwards', 'temperature_of_snow_layer', 'total_evaporation',
             'total_precipitation', 'volumetric_soil_water_layer_1', 'volumetric_soil_water_layer_2',
             'volumetric_soil_water_layer_3', 'volumetric_soil_water_layer_4',
+        ],
+    'reanalysis-era5-pressure-levels': [
+            'divergence', 'fraction_of_cloud_cover', 'geopotential',
+            'ozone_mass_mixing_ratio', 'potential_vorticity', 'relative_humidity',
+            'specific_cloud_ice_water_content', 'specific_cloud_liquid_water_content', 'specific_humidity',
+            'specific_rain_water_content', 'specific_snow_water_content', 'temperature',
+            'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity',
+            'vorticity',
+        ],
+    'reanalysis-era5-single-levels': [
+            '100m_u_component_of_wind', '100m_v_component_of_wind', '10m_u_component_of_neutral_wind',
+            '10m_u_component_of_wind', '10m_v_component_of_neutral_wind', '10m_v_component_of_wind',
+            '10m_wind_gust_since_previous_post_processing', '2m_dewpoint_temperature', '2m_temperature',
+            'air_density_over_the_oceans', 'angle_of_sub_gridscale_orography', 'anisotropy_of_sub_gridscale_orography',
+            'benjamin_feir_index', 'boundary_layer_dissipation', 'boundary_layer_height',
+            'charnock', 'clear_sky_direct_solar_radiation_at_surface', 'cloud_base_height',
+            'coefficient_of_drag_with_waves', 'convective_available_potential_energy', 'convective_inhibition',
+            'convective_precipitation', 'convective_rain_rate', 'convective_snowfall',
+            'convective_snowfall_rate_water_equivalent', 'downward_uv_radiation_at_the_surface', 'duct_base_height',
+            'eastward_gravity_wave_surface_stress', 'eastward_turbulent_surface_stress', 'evaporation',
+            'forecast_albedo', 'forecast_logarithm_of_surface_roughness_for_heat', 'forecast_surface_roughness',
+            'free_convective_velocity_over_the_oceans', 'friction_velocity', 'geopotential',
+            'gravity_wave_dissipation', 'high_cloud_cover', 'high_vegetation_cover',
+            'ice_temperature_layer_1', 'ice_temperature_layer_2', 'ice_temperature_layer_3',
+            'ice_temperature_layer_4', 'instantaneous_10m_wind_gust', 'instantaneous_eastward_turbulent_surface_stress',
+            'instantaneous_large_scale_surface_precipitation_fraction', 'instantaneous_moisture_flux', 'instantaneous_northward_turbulent_surface_stress',
+            'instantaneous_surface_sensible_heat_flux', 'k_index', 'lake_bottom_temperature',
+            'lake_cover', 'lake_depth', 'lake_ice_depth',
+            'lake_ice_temperature', 'lake_mix_layer_depth', 'lake_mix_layer_temperature',
+            'lake_shape_factor', 'lake_total_layer_temperature', 'land_sea_mask',
+            'large_scale_precipitation', 'large_scale_precipitation_fraction', 'large_scale_rain_rate',
+            'large_scale_snowfall', 'large_scale_snowfall_rate_water_equivalent', 'leaf_area_index_high_vegetation',
+            'leaf_area_index_low_vegetation', 'low_cloud_cover', 'low_vegetation_cover',
+            'maximum_2m_temperature_since_previous_post_processing', 'maximum_individual_wave_height', 'maximum_total_precipitation_rate_since_previous_post_processing',
+            'mean_boundary_layer_dissipation', 'mean_convective_precipitation_rate', 'mean_convective_snowfall_rate',
+            'mean_direction_of_total_swell', 'mean_direction_of_wind_waves', 'mean_eastward_gravity_wave_surface_stress',
+            'mean_eastward_turbulent_surface_stress', 'mean_evaporation_rate', 'mean_gravity_wave_dissipation',
+            'mean_large_scale_precipitation_fraction', 'mean_large_scale_precipitation_rate', 'mean_large_scale_snowfall_rate',
+            'mean_northward_gravity_wave_surface_stress', 'mean_northward_turbulent_surface_stress', 'mean_period_of_total_swell',
+            'mean_period_of_wind_waves', 'mean_potential_evaporation_rate', 'mean_runoff_rate',
+            'mean_sea_level_pressure', 'mean_snow_evaporation_rate', 'mean_snowfall_rate',
+            'mean_snowmelt_rate', 'mean_square_slope_of_waves', 'mean_sub_surface_runoff_rate',
+            'mean_surface_direct_short_wave_radiation_flux', 'mean_surface_direct_short_wave_radiation_flux_clear_sky', 'mean_surface_downward_long_wave_radiation_flux',
+            'mean_surface_downward_long_wave_radiation_flux_clear_sky', 'mean_surface_downward_short_wave_radiation_flux', 'mean_surface_downward_short_wave_radiation_flux_clear_sky',
+            'mean_surface_downward_uv_radiation_flux', 'mean_surface_latent_heat_flux', 'mean_surface_net_long_wave_radiation_flux',
+            'mean_surface_net_long_wave_radiation_flux_clear_sky', 'mean_surface_net_short_wave_radiation_flux', 'mean_surface_net_short_wave_radiation_flux_clear_sky',
+            'mean_surface_runoff_rate', 'mean_surface_sensible_heat_flux', 'mean_top_downward_short_wave_radiation_flux',
+            'mean_top_net_long_wave_radiation_flux', 'mean_top_net_long_wave_radiation_flux_clear_sky', 'mean_top_net_short_wave_radiation_flux',
+            'mean_top_net_short_wave_radiation_flux_clear_sky', 'mean_total_precipitation_rate', 'mean_vertical_gradient_of_refractivity_inside_trapping_layer',
+            'mean_vertically_integrated_moisture_divergence', 'mean_wave_direction', 'mean_wave_direction_of_first_swell_partition',
+            'mean_wave_direction_of_second_swell_partition', 'mean_wave_direction_of_third_swell_partition', 'mean_wave_period',
+            'mean_wave_period_based_on_first_moment', 'mean_wave_period_based_on_first_moment_for_swell', 'mean_wave_period_based_on_first_moment_for_wind_waves',
+            'mean_wave_period_based_on_second_moment_for_swell', 'mean_wave_period_based_on_second_moment_for_wind_waves', 'mean_wave_period_of_first_swell_partition',
+            'mean_wave_period_of_second_swell_partition', 'mean_wave_period_of_third_swell_partition', 'mean_zero_crossing_wave_period',
+            'medium_cloud_cover', 'minimum_2m_temperature_since_previous_post_processing', 'minimum_total_precipitation_rate_since_previous_post_processing',
+            'minimum_vertical_gradient_of_refractivity_inside_trapping_layer', 'model_bathymetry', 'near_ir_albedo_for_diffuse_radiation',
+            'near_ir_albedo_for_direct_radiation', 'normalized_energy_flux_into_ocean', 'normalized_energy_flux_into_waves',
+            'normalized_stress_into_ocean', 'northward_gravity_wave_surface_stress', 'northward_turbulent_surface_stress',
+            'ocean_surface_stress_equivalent_10m_neutral_wind_direction', 'ocean_surface_stress_equivalent_10m_neutral_wind_speed', 'peak_wave_period',
+            'period_corresponding_to_maximum_individual_wave_height', 'potential_evaporation', 'precipitation_type',
+            'runoff', 'sea_ice_cover', 'sea_surface_temperature',
+            'significant_height_of_combined_wind_waves_and_swell', 'significant_height_of_total_swell', 'significant_height_of_wind_waves',
+            'significant_wave_height_of_first_swell_partition', 'significant_wave_height_of_second_swell_partition', 'significant_wave_height_of_third_swell_partition',
+            'skin_reservoir_content', 'skin_temperature', 'slope_of_sub_gridscale_orography',
+            'snow_albedo', 'snow_density', 'snow_depth',
+            'snow_evaporation', 'snowfall', 'snowmelt',
+            'soil_temperature_level_1', 'soil_temperature_level_2', 'soil_temperature_level_3',
+            'soil_temperature_level_4', 'soil_type', 'standard_deviation_of_filtered_subgrid_orography',
+            'standard_deviation_of_orography', 'sub_surface_runoff', 'surface_latent_heat_flux',
+            'surface_net_solar_radiation', 'surface_net_solar_radiation_clear_sky', 'surface_net_thermal_radiation',
+            'surface_net_thermal_radiation_clear_sky', 'surface_pressure', 'surface_runoff',
+            'surface_sensible_heat_flux', 'surface_solar_radiation_downward_clear_sky', 'surface_solar_radiation_downwards',
+            'surface_thermal_radiation_downward_clear_sky', 'surface_thermal_radiation_downwards', 'temperature_of_snow_layer',
+            'toa_incident_solar_radiation', 'top_net_solar_radiation', 'top_net_solar_radiation_clear_sky',
+            'top_net_thermal_radiation', 'top_net_thermal_radiation_clear_sky', 'total_cloud_cover',
+            'total_column_cloud_ice_water', 'total_column_cloud_liquid_water', 'total_column_ozone',
+            'total_column_rain_water', 'total_column_snow_water', 'total_column_supercooled_liquid_water',
+            'total_column_water', 'total_column_water_vapour', 'total_precipitation',
+            'total_sky_direct_solar_radiation_at_surface', 'total_totals_index', 'trapping_layer_base_height',
+            'trapping_layer_top_height', 'type_of_high_vegetation', 'type_of_low_vegetation',
+            'u_component_stokes_drift', 'uv_visible_albedo_for_diffuse_radiation', 'uv_visible_albedo_for_direct_radiation',
+            'v_component_stokes_drift', 'vertical_integral_of_divergence_of_cloud_frozen_water_flux', 'vertical_integral_of_divergence_of_cloud_liquid_water_flux',
+            'vertical_integral_of_divergence_of_geopotential_flux', 'vertical_integral_of_divergence_of_kinetic_energy_flux', 'vertical_integral_of_divergence_of_mass_flux',
+            'vertical_integral_of_divergence_of_moisture_flux', 'vertical_integral_of_divergence_of_ozone_flux', 'vertical_integral_of_divergence_of_thermal_energy_flux',
+            'vertical_integral_of_divergence_of_total_energy_flux', 'vertical_integral_of_eastward_cloud_frozen_water_flux', 'vertical_integral_of_eastward_cloud_liquid_water_flux',
+            'vertical_integral_of_eastward_geopotential_flux', 'vertical_integral_of_eastward_heat_flux', 'vertical_integral_of_eastward_kinetic_energy_flux',
+            'vertical_integral_of_eastward_mass_flux', 'vertical_integral_of_eastward_ozone_flux', 'vertical_integral_of_eastward_total_energy_flux',
+            'vertical_integral_of_eastward_water_vapour_flux', 'vertical_integral_of_energy_conversion', 'vertical_integral_of_kinetic_energy',
+            'vertical_integral_of_mass_of_atmosphere', 'vertical_integral_of_mass_tendency', 'vertical_integral_of_northward_cloud_frozen_water_flux',
+            'vertical_integral_of_northward_cloud_liquid_water_flux', 'vertical_integral_of_northward_geopotential_flux', 'vertical_integral_of_northward_heat_flux',
+            'vertical_integral_of_northward_kinetic_energy_flux', 'vertical_integral_of_northward_mass_flux', 'vertical_integral_of_northward_ozone_flux',
+            'vertical_integral_of_northward_total_energy_flux', 'vertical_integral_of_northward_water_vapour_flux', 'vertical_integral_of_potential_and_internal_energy',
+            'vertical_integral_of_potential_internal_and_latent_energy', 'vertical_integral_of_temperature', 'vertical_integral_of_thermal_energy',
+            'vertical_integral_of_total_energy', 'vertically_integrated_moisture_divergence', 'volumetric_soil_water_layer_1',
+            'volumetric_soil_water_layer_2', 'volumetric_soil_water_layer_3', 'volumetric_soil_water_layer_4',
+            'wave_spectral_directional_width', 'wave_spectral_directional_width_for_swell', 'wave_spectral_directional_width_for_wind_waves',
+            'wave_spectral_kurtosis', 'wave_spectral_peakedness', 'wave_spectral_skewness',
+            'zero_degree_level',
         ]}
+
+pressure_levels = {'reanalysis-era5-pressure-levels':  [
+                1, 2, 3,
+                5, 7, 10,
+                20, 30, 50,
+                70, 100, 125,
+                150, 175, 200,
+                225, 250, 300,
+                350, 400, 450,
+                500, 550, 600,
+                650, 700, 750,
+                775, 800, 825,
+                850, 875, 900,
+                925, 950, 975,
+                1000,
+            ]
+    }
+
+product_types = {'reanalysis-era5-single-levels': [
+            'ensemble_mean', 'ensemble_members', 'ensemble_spread',
+            'reanalysis'],
+    'reanalysis-era5-pressure-levels': [
+                'ensemble_mean', 'ensemble_members', 'ensemble_spread',
+                'reanalysis']
+    }
 
 ########################################
 ### Helper functions
@@ -181,15 +304,18 @@ class CDS(object):
         client = cdsapi.Client(url=url, key=key, session=sess)
 
         setattr(self, 'client', client)
-        setattr(self, 'available_parameters', available_parameters)
-        setattr(self, 'available_products', list(available_parameters.keys()))
+        setattr(self, 'available_variables', available_variables)
+        setattr(self, 'available_products', list(available_variables.keys()))
         setattr(self, 'available_freq_intervals', available_freq_intervals)
+        setattr(self, 'available_product_types', product_types)
+        setattr(self, 'available_pressure_levels', pressure_levels)
         setattr(self, 'threads', threads)
 
 
-    def download(self, product, parameters, from_date, to_date, bbox, freq_interval='10Y'):
+    def download(self, product: str, variables, from_date, to_date, bbox, freq_interval='10Y', product_types=None, pressure_levels=None):
         """
         The method to do the actual downloading of the files. The current maximum queue limit is 32 requests per user and this has been set as the number of threads to use. The cdsapi blocks the threads until they finished downloading. This can take a very long time for many large files...make sure this process can run happily without interruption for a while...
+        This method does not check to make sure you do not exceede the CDS extraction limit of 120,000 values.
 
         The freq_interval can be 1D or D for daily, 1M or M for monthly, or yearly (Y) with up to 11 years (11Y).
         This extraction resolution is due to the limitations of the cdsapi.
@@ -198,8 +324,8 @@ class CDS(object):
         ----------
         product : str
             The requested product. Look at the available_parameters keys for all options.
-        parameters : str or list of str
-            The requested parameters. Look at the available_parameters values for all options.
+        variables : str or list of str
+            The requested variables. Look at the available_variables values for all options.
         from_date : str or Timestamp
             The start date of the extraction.
         to_date : str or Timestamp
@@ -208,29 +334,62 @@ class CDS(object):
             The bounding box of lat and lon for the requested area. It must be in the order of [upper lat, left lon, lower lat, right lon].
         freq_interval : str
             Pandas frequency string representing the time interval of each request. The freq_interval can be 1D or D for daily, 1M or M for monthly, or yearly (Y) with up to 11 years (11Y).
+        product_types : str or list of str
+            Some products have product types and if they do they need to be specified. Check the available_product_types object for the available options.
+        pressure_levels : int or list of int
+            Some products have pressure levels and if they do they need to be specified. Check the available_pressure_levels object for the available options.
 
         Returns
         -------
         None
         """
         ## Run checks
-        if product not in available_parameters.keys():
+        # Products
+        if product not in self.available_variables.keys():
             raise ValueError('product is not available.')
 
-        if isinstance(parameters, str):
-            params = [parameters]
-        elif isinstance(parameters, list):
-            params = parameters.copy()
+        # Parameters/variables
+        if isinstance(variables, str):
+            params = [variables]
+        elif isinstance(variables, list):
+            params = variables.copy()
         else:
-            raise TypeError('parameters must be a str or a list of str.')
+            raise TypeError('variables must be a str or a list of str.')
 
         for p in params:
-            av = self.available_parameters[product]
+            av = self.available_variables[product]
             if not p in av:
-                raise ValueError(p + ' is not one of the available parameters for this product.')
+                raise ValueError(p + ' is not one of the available variables for this product.')
 
+        # freq intervals
         if not freq_interval in self.available_freq_intervals:
             raise ValueError('freq_interval must be one of: ' + str(self.available_freq_intervals))
+
+        # Product types
+        if product in self.available_product_types:
+            if isinstance(product_types, str):
+                product_types1 = [product_types]
+            elif not isinstance(product_types, list):
+                raise TypeError('The requested product has required product_types, but none have been specified.')
+            pt_bool = all([p in self.available_product_types[product] for p in product_types1])
+
+            if not pt_bool:
+                raise ValueError('Not all requested product_types are in the available_product_types.')
+        else:
+            product_types1 = None
+
+        # Pressure levels
+        if product in self.available_pressure_levels:
+            if isinstance(pressure_levels, int):
+                pressure_levels1 = [pressure_levels]
+            elif not isinstance(pressure_levels, list):
+                raise TypeError('The requested product has required pressure_levels, but none have been specified.')
+            pl_bool = all([p in self.available_pressure_levels[product] for p in pressure_levels1])
+
+            if not pl_bool:
+                raise ValueError('Not all requested pressure_levels are in the available_pressure_levels.')
+        else:
+            pressure_levels1 = None
 
         ## Parse dates
         if isinstance(from_date, (str, pd.Timestamp)):
@@ -262,6 +421,12 @@ class CDS(object):
         req_list = []
         for p in params:
             dict1 = {'format': 'netcdf', 'variable': p, 'area': bbox1}
+
+            if isinstance(product_types1, list):
+                dict1['product_type'] = product_types1
+
+            if isinstance(pressure_levels1, list):
+                dict1['pressure_level'] = [str(p) for p in pressure_levels1]
 
             for i, tdate in enumerate(dates1[1:]):
                 fdate = dates1[i]
